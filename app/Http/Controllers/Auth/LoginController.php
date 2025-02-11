611feeb82fa672e8d2e7dd157242fe83065f509a
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2025-02-11 08:33:47 +0800

    fix: username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,6 +7,7 @@ namespace App\Http\Controllers\Auth;
 use App\Enums\UserStatus;
 use App\Enums\UserType;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
@@ -16,14 +17,14 @@ final class LoginController
     /**
      * Handle an authentication attempt.
      */
-    public function authenticate(Request $request): \Illuminate\Http\JsonResponse
+    public function authenticate(Request $request): JsonResponse
     {
         $request->validate([
             'username' => 'required|string',
             'password' => 'required',
         ]);
 
-        $user = User::query()->where('username', $request->email)
+        $user = User::query()->where('username', $request->username)
             ->where('status', UserStatus::ACTIVE)
             ->where('user_type', UserType::TELLER)
             ->first();
