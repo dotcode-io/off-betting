d70b6f92c27ed73f64cc05fd68c6f538bcc812ba
Ivan Krister Garcia <ivankrister.garcia@gmail.com>
2025-03-24 21:40:49 +0800

    fix: db seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,7 @@ use App\Models\AppSetting;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 final class DatabaseSeeder extends Seeder
 {
@@ -18,19 +19,29 @@ final class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
+        User::create([
             'username' => 'admin',
             'user_type' => 'admin',
+            Hash::make('password'),
+            'wallet_amount' => 0,
+            'commission_amount' => 0,
+
         ]);
 
-        User::factory()->create([
+        User::create([
             'username' => 'teller',
             'user_type' => 'teller',
+            Hash::make('password'),
+            'wallet_amount' => 0,
+            'commission_amount' => 0,
         ]);
 
-        User::factory()->create([
+        User::create([
             'username' => 'controller',
             'user_type' => 'controller',
+            Hash::make('password'),
+            'wallet_amount' => 0,
+            'commission_amount' => 0,
         ]);
 
         AppSetting::create([
